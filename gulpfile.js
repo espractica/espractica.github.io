81498c5790c43b6d76a3f3f1d049a9cb2fa9a72d
Fabian <fega.hg@gmail.com>
2016-05-10 14:52:03 -0500

    a lot of boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+var gulp = require('gulp');
+var hb = require('gulp-hb');
+var csso = require('gulp-csso');
+var concat = require('gulp-concat');
+var csslint = require('gulp-csslint');
+var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var svgmin = require('gulp-svgmin');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var htmlmin = require('gulp-htmlmin');
+var w3cjs = require('gulp-w3cjs');
+var frontMatter = require('gulp-front-matter');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var sourcemaps = require('gulp-sourcemaps');
+var ghPages = require('gulp-gh-pages')
+
+function plumberit(errTitle) {
+return plumber({
+errorHandler: notify.onError({
+    title: errTitle || "Error running Gulp",
+    message: "Error: <%= error.message %>",
+    })
+});
+}
+/*///////////////////////////////////////
+Browser Sync Implementation
+///////////////////////////////////////*/
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+/*///////////////////////////////////////
+build and deploy
+///////////////////////////////////////*/
+gulp.task('build', function () {
+    return gulp
+        .src('./src/views/pages/*.html')
+        .pipe(plumberit('Build Error'))
+        .pipe(frontMatter({property: 'data.front' }))
+        .pipe(hb({partials: './src/views/partials/*.hbs',data: './src/views/data.json',debug:1}))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({'branch':'master'}));
+}); 
+/*///////////////////////////////////////
+linters
+///////////////////////////////////////*/
+gulp.task('jslint', function() {
+  return gulp.src('./src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+gulp.task('csslint', function() {
+  gulp.src(['./src/css/style.css'])
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+});
+gulp.task('htmllint', function() {
+  gulp.src(['./dist/*.html'])
+    .pipe(plumberit("HTML Validation errors"))
+    .pipe(w3cjs()) 
+    //.pipe(w3cjs.reporter());
+});
+/*///////////////////////////////////////
+JS watcher 
+///////////////////////////////////////*/
+gulp.task('js',  function () {
+  return gulp.src(['./src/libraries/*.js','./src/js/*.js'])
+    .pipe(sourcemaps.init())
+        .pipe(plumberit("JS uglify errors"))
+        .pipe(concat('min.js'))
+        .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+/*///////////////////////////////////////
+CSS watcher 
+///////////////////////////////////////*/
+gulp.task('css', function () { 
+  return gulp.src(['./src/css/materialize.css','./src/css/*.css'])
+        .pipe(sourcemaps.init())
+            .pipe(plumberit('CSS parsing error'))
+            .pipe(csso())
+            .pipe(concat('min.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+/*///////////////////////////////////////
+img minifier 
+///////////////////////////////////////*/
+gulp.task('svg', function () {
+    return gulp.src('./src/img/**/*.svg')
+        .pipe(plumberit('Svg minification error'))
+        .pipe(svgmin())
+        .pipe(gulp.dest('./dist/img'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+gulp.task('img', function(){
+	return gulp.src('./src/img/**/*.+(png|jpg|jpeg|gif)')
+    .pipe(plumberit('Img minification error'))
+	.pipe(cache(imagemin({interlaced: true})))// Caching images that ran through imagemin
+	.pipe(gulp.dest('dist/img'))
+});
+/*///////////////////////////////////////
+copy fonts
+///////////////////////////////////////*/
+gulp.task('font', function () {
+    return gulp.src('./src/font/**/*')
+        .pipe(gulp.dest('./dist/font'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+/*///////////////////////////////////////
+RELOAD YOUR BROWSER
+///////////////////////////////////////*/
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+/*///////////////////////////////////////
+WATCHER
+///////////////////////////////////////*/
+gulp.task('default', ['browser-sync','js','css','build','svg','img'], function () {
+	gulp.watch(['./src/js/*.js'],   ['js','jslint']);
+	gulp.watch('./src/**/*.css',  ['css','csslint']);
+	gulp.watch('./dist/**/*.html', ['bs-reload','htmllint']);
+	gulp.watch(['./src/views/**/*'], ['build']);
+	gulp.watch('./src/**/*.svg', ['svg']);
+	gulp.watch('src/**/*.+(png|jpg|jpeg|gif)', ['img']);
+});
---
+++ src/views/licenses/licenses.js
@@ -0,0 +1,51 @@
+module.exports =[{
+	'title':'MaterializeCSS',
+	'description':'A modern responsive front-end framework based on Material Design',
+	'license':'The MIT License (MIT), Copyright (c) 2014-2016 Materialize',
+	'img':'./img/licenses/materialize.svg'
+},{
+	'title':'Jquery',
+	'description':'jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript.',
+	'license':'The MIT License (MIT)',
+	'img':'./img/licenses/jquery.svg'
+},{
+	'title':'ContentTools',
+	'description':'A JavaScript library aimed at building WYSIWYG editors for HTML content.',
+	'license':'The MIT License (MIT), Copyright (c) 2014 Getme Limited (http://getme.co.uk)',
+	'img':'./img/licenses/c-tools.png'
+},{
+	'title':'Mathjax',
+	'description':'Beautiful math in all browsers. A JavaScript display engine for mathematics that works in all browsers. No more setup for readers. It just works.',
+	'license':'Apache License Version 2.0, January 2004,MathJax ©2009-2016',
+	'img':'./img/licenses/mathjax.svg'
+},{
+	'title':'P5.js',
+	'description':'p5.js a JS client-side library for creating graphic and interactive experiences, based on the core principles of Processing.',
+	'license':'GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999',
+	'img':'./img/licenses/p5js.svg'
+},{
+	'title':'chartist.js',
+	'description':'Simple Responsive Charts',
+	'license':' DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, Version 2, December 2004'
+},{
+	'title':'charts.js',
+	'description':'Simple HTML5 Charts using the canvas element chartjs.org',
+	'license':' The MIT License (MIT) Copyright (c) 2013-2016 Nick Downie'
+},{
+	'title':'Highlight.js',
+	'description':'Highlight.js is a syntax highlighter written in JavaScript. It works in the browser as well as on the server',
+	'license':' Copyright (c) 2006, Ivan Sagalaev All rights reserved. License: (https://github.com/isagalaev/highlight.js/blob/master/LICENSE)'
+},{
+	'title':'page.js',
+	'description':'Micro client-side router inspired by the Express router (~1200 bytes)',
+	'license':'The MIT License, Copyright (c) 2012 TJ Holowaychuk <tj@vision-media.ca>'
+},{
+	'title':'jQuery.form.js',
+	'description':'jQuery Form Plugin http://jquery.malsup.com/form/',
+	'license':'Copyright 2006-2013 (c) M. Alsup, Dual licensed under the MIT and GPL licenses'
+},{
+	'title':'codemirror.js',
+	'description':'CodeMirror is a versatile text editor implemented in JavaScript for the browser. It is specialized for editing code, and comes with a number of language modes and addons that implement more advanced editing functionality.',
+	'license':'Copyright (C) 2016 by Marijn Haverbeke <marijnh@gmail.com> and others License: (https://github.com/codemirror/CodeMirror/blob/master/LICENSE)'
+	'img':'./img/licenses/code-mirror.png'
+}]
